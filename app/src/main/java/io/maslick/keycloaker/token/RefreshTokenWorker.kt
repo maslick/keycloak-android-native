cf9f1294dcea932c013b0a3304f0532485caff31
Pavel Maslov <pavel.masloff@gmail.com>
2018-12-30 11:46:47 +0100

    small fixes

--- app/src/main/java/io/maslick/keycloaker/token/RefreshTokenWorker.kt
+++ app/src/main/java/io/maslick/keycloaker/token/RefreshTokenWorker.kt
@@ -2,6 +2,7 @@ package io.maslick.keycloaker.token
 
 import android.content.Context
 import androidx.work.*
+import androidx.work.ExistingPeriodicWorkPolicy.REPLACE
 import io.maslick.keycloaker.Config
 import io.maslick.keycloaker.di.IKeycloakRest
 import io.maslick.keycloaker.di.KeycloakToken
@@ -9,10 +10,9 @@ import io.maslick.keycloaker.storage.IOAuth2AccessTokenStorage
 import org.koin.standalone.KoinComponent
 import org.koin.standalone.inject
 import java.util.*
-import java.util.concurrent.TimeUnit
+import java.util.concurrent.TimeUnit.MINUTES
 
-class RefreshTokenWorker(appContext: Context, workerParameters: WorkerParameters): Worker(appContext, workerParameters),
-    KoinComponent {
+class RefreshTokenWorker(context: Context, params: WorkerParameters): Worker(context, params), KoinComponent {
 
     companion object {
         fun startPeriodicRefreshTokenTask() {
@@ -20,32 +20,31 @@ class RefreshTokenWorker(appContext: Context, workerParameters: WorkerParameters
                 .setRequiredNetworkType(NetworkType.CONNECTED)
                 .build()
 
-            val periodicRefreshTokenWork = PeriodicWorkRequest.Builder(RefreshTokenWorker::class.java, 15, TimeUnit.MINUTES)
+            val periodicWork = PeriodicWorkRequest.Builder(RefreshTokenWorker::class.java, 15, MINUTES)
                 .setConstraints(workConstraints)
                 .build()
 
-            WorkManager.getInstance().enqueueUniquePeriodicWork("kc-refresh-token-work",
-                ExistingPeriodicWorkPolicy.REPLACE, periodicRefreshTokenWork)
+            WorkManager.getInstance().enqueueUniquePeriodicWork("kc-refresh-token-work", REPLACE, periodicWork)
         }
     }
 
-    override fun doWork(): Result {
-        val api: IKeycloakRest by inject()
-        val storage: IOAuth2AccessTokenStorage by inject()
+    private val api: IKeycloakRest by inject()
+    private val storage: IOAuth2AccessTokenStorage by inject()
 
+    override fun doWork(): Result {
         if (storage.getStoredAccessToken() == null || storage.getStoredAccessToken()!!.refreshToken == null)
             return Result.failure()
 
         return try {
             val token = api.refreshAccessToken(storage.getStoredAccessToken()!!.refreshToken!!, Config.clientId).blockingFirst()
-            saveTokenToStorage(token, storage)
+            saveTokenToStorage(token)
             Result.success()
         } catch (e: Exception) {
             Result.retry()
         }
     }
 
-    private fun saveTokenToStorage(token: KeycloakToken, storage: IOAuth2AccessTokenStorage) {
+    private fun saveTokenToStorage(token: KeycloakToken) {
         val expirationDate = Calendar.getInstance().clone() as Calendar
         val refreshExpirationDate = Calendar.getInstance().clone() as Calendar
         expirationDate.add(Calendar.SECOND, token.expiresIn!!)
